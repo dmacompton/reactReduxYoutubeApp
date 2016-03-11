226a97fc76b31f2e30e85a4e841cf3b9b9d78ed4
dmacompton <d.mihaylov@ringostat.com>
2016-03-11 08:40:44 +0200

    #28

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({ video }) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    
     return (
-        <li>Video</li>
-    )
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="medial-left">
+                    <img className="media-object" src={imageUrl} alt="" />
+                </div>
+                
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBIb0tM2WoWGd8X9kGsl40Oov-cdSqFWhM';
 
@@ -13,7 +14,7 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+        YTSearch({key: API_KEY, term: "dmacompton"}, (videos) => {
             this.setState({ videos });
             //this.setState({ videos: videos });
         });
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );
